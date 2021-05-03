8dce82de53cf8bfabcad797a1c4fdbe1c9c620be
Dan Ross <daniellross@gmail.com>
2021-05-03 16:00:15 -0500

    added models

---
+++ levelupapi/models/event.py
@@ -0,0 +1,11 @@
+from levelupapi.models.gamer import Gamer
+from levelupapi.models.games import Game
+from django.db import models
+
+class Event(models.Model):
+
+    organizer = models.ForeignKey('Gamer', on_delete=models.CASCADE)
+    game = models.ForeignKey('Game', on_delete=models.CASCADE)
+    date = models.DateField
+    time = models.TimeField
+    address = models.CharField
