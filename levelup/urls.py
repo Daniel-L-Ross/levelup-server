e42e668e33c0c45d3545d22ef33d64f1611113c2
Dan Ross <daniellross@gmail.com>
2021-05-05 15:49:51 -0500

    Added viewset for genres

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,11 +1,16 @@
+from django import urls
 from django.conf.urls import include
 from django.urls import path
-from levelupapi.views import register_user, login_user
+from levelupapi.views import register_user, login_user, Genres
+from rest_framework import routers
+
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'genres', Genres, 'genre')
 
 urlpatterns = [
-    # Requests to http://localhost:8000/register will be routed to the register_user function
     path('register', register_user),
-    # Requests to http://localhost:8000/login will be routed to the login_user function
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include(router.urls))
 ]
