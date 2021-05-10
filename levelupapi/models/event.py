bf1698b382b55ec9227e0450d1d70ee92623bbc3
Dan Ross <daniellross@gmail.com>
2021-05-10 15:26:40 -0500

    updated event model with custom properties for joined. Added logic at event view for GET method

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -8,3 +8,11 @@ class Event(models.Model):
     time = models.TimeField()
     description = models.TextField()
     attendees = models.ManyToManyField('Gamer', through='GamerEvent', related_name='attending')
+
+    @property
+    def joined(self):
+        return self.__joined
+
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
