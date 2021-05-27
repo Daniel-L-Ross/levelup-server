a0bcf1563e3a236c0826024f95c57c6916fe9790
Dan Ross <daniellross@gmail.com>
2021-05-27 14:36:30 -0500

    added template report for events by user. working on html formatting

--- levelupreports/urls.py
+++ levelupreports/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.urls.conf import include
-from .views import usergame_list
+from .views import usergame_list, userevent_list
 
 urlpatterns = [
     path('reports/usergames', usergame_list),
+    path('reports/userevents', userevent_list)
 ]
