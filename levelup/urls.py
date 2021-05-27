cdc8ca4cb8218480efdebcd8f6a85a0156072761
Dan Ross <daniellross@gmail.com>
2021-05-27 15:59:07 -0500

    Added admin path back to ursl, created new sueruser

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,4 +1,5 @@
 from django import urls
+from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user, GenreView, GameView, EventView, ProfileView
@@ -12,6 +13,7 @@ router.register(r'events', EventView, 'event')
 router.register(r'profile', ProfileView, 'profile')
 
 urlpatterns = [
+    path('admin', admin.site.urls),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
